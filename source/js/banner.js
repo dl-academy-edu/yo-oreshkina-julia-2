e388aef7166d43cf21809c6f01c66fbadf7ca771
FilosofiaJulia <filosofiajulia@gmail.com>
2023-01-29 18:43:30 +0300

    fixed banner
    Изменила название классов по БЭМ, стили по правилам scss

--- source/js/banner.js
+++ source/js/banner.js
@@ -1,30 +1,30 @@
 window.addEventListener('DOMContentLoaded', function(){ 
 
     const banner = document.querySelector('.banner');
-    const bannerTitle = document.querySelector('.banner__slide-title');
-    const numberOfSlide = document.getElementById('span');
-    const bannerInfo = document.querySelector('.banner__slide-info');
-    const switchButtons = document.querySelectorAll('.switch__btn');
+    const bannerTitle = document.querySelector('.banner__title');
+    const numberOfSlide = document.querySelector('.banner__slide-number');
+    const bannerSubtitle = document.querySelector('.banner__subtitle');
+    const switchButtons = document.querySelectorAll('.banner__switch-btn');
 
     const slidesDB = [
         {   number: '01',
             title:'The ooh noo collection',
-            info: 'Designed in Slovenia',
+            subtitle: 'Designed in Slovenia',
             color: '#EEE4DB'
         },
         {   number: '02',
             title:'the funny collection',
-            info: 'Designed in Moldova',
+            subtitle: 'Designed in Moldova',
             color: 'rgba(118, 194, 116, 0.5)'
         },
         {   number: '03',
             title:'the scary collection',
-            info: 'Designed in Serbia',
+            subtitle: 'Designed in Serbia',
             color: 'rgba(192, 116, 194, 0.5)'
         },
         {   number: '04',
             title:'The teen collection',
-            info: 'Designed in Belarus',
+            subtitle: 'Designed in Belarus',
             color: 'rgba(116, 157, 194, 0.5)'
         }
     ];
@@ -33,13 +33,13 @@ window.addEventListener('DOMContentLoaded', function(){
         for (let i = 0; i < switchButtons.length; i++) { 
             switchButtons[i].addEventListener('click', function(e){ 
                 switchButtons.forEach(button => {
-                    button.classList.remove('btn-active'); 
+                    button.classList.remove('banner__switch-btn--active'); 
                 });
-                switchButtons[i].classList.add('btn-active');
+                switchButtons[i].classList.add('banner__switch-btn--active');
                 banner.style.backgroundColor = arr[i].color;
                 numberOfSlide.textContent = arr[i].number;
                 bannerTitle.textContent = arr[i].title;
-                bannerInfo.textContent = arr[i].info;
+                bannerSubtitle.textContent = arr[i].subtitle;
             }); 
         } 
     }
